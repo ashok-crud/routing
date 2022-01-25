7b3b23e31ad4ac1ab4aa2cf4fd72a884bc797c47
ashok-crud <85573150+ashok-crud@users.noreply.github.com>
2022-01-25 09:38:15 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+const appRoutes : Routes = [
+  {path :'', component:HomeComponent},
+  {path :'users', component:UsersComponent},
+  {path :'users/:id/:name', component:UserComponent},
+  {path:'categories', component:CategoriesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    CategoriesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
